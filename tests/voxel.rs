6b9c3285a5c9addb225228692b36f91ecb681293
William Cahill <drewlikesramen@gmail.com>
2021-10-15 22:24:40 -0400

    Added selections. Working on insertions

--- tests/voxel.rs
+++ tests/voxel.rs
@@ -1,22 +1,52 @@
-use tile_editor::voxel::{VoxelMap, Coords, Selection, Chunk};
+use tile_editor::voxel::{VoxelMap, Coords, Selection, RawChunk, Chunk, Size, Slot};
 
 #[test]
-fn new() {
-    let mut map = VoxelMap::new(32, 32, 32);
+fn test_chunk_iterator() {
+    let mut map = VoxelMap::new(Size::new(32, 32, 32));
     let chunks = map
         .select_chunks(Selection::new(0, 0, 0, 1, 1, 1))
-        .collect::<Vec<&mut Chunk>>();
+        .collect::<Vec<Chunk>>();
     assert_eq!(8, chunks.len());
     let chunks = map
         .select_chunks(Selection::new(1, 1, 1, 2, 2, 2))
-        .collect::<Vec<&mut Chunk>>();
+        .collect::<Vec<Chunk>>();
     assert_eq!(8, chunks.len());
     let chunks = map
-        .select_chunks(Selection::new(-1, -1, -1, -2, -2, -2))
-        .collect::<Vec<&mut Chunk>>();
-    assert_eq!(8, chunks.len());
-    let chunks = map
-        .select_chunks(Selection::new(-1, -1, -1, -3, -2, -2))
-        .collect::<Vec<&mut Chunk>>();
+        .select_chunks(Selection::new(-3, -3, -3, -1, -2, -2))
+        .collect::<Vec<Chunk>>();
     assert_eq!(12, chunks.len());
 }
+
+#[test]
+fn test_slot_iterator() {
+    let mut map = VoxelMap::new(Size::new(32, 32, 32));
+    let expected = [
+        Coords { x: -1, y: -1, z: -1 },
+        Coords { x: 0, y: -1, z: -1 },
+        Coords { x: -1, y: 0, z: -1 },
+        Coords { x: 0, y: 0, z: -1 },
+        Coords { x: -1, y: -1, z: 0 },
+        Coords { x: 0, y: -1, z: 0 },
+        Coords { x: -1, y: 0, z: 0 },
+        Coords { x: 0, y: 0, z: 0 },
+    ];
+    let actual = map
+        .select_slots(Selection::new(-1, -1, -1, 0, 0, 0))
+        .map(|slot| slot.coords())
+        .collect::<Vec<Coords>>();
+    let actual = &actual[..];
+    assert_eq!(expected, actual);
+}
+
+/*
+#[test]
+fn test_slot_insert() {
+    let mut map = VoxelMap::new(Size::new(32, 32, 32));
+    let actual = map
+        .select_slots(Selection::new(-1, -1, -1, 0, 0, 0))
+        .map(|slot| slot.coords())
+        .collect::<Vec<Coords>>();
+    let actual = &actual[..];
+    assert_eq!(expected, actual);
+}
+ */
