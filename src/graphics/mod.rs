c44154a079901f445c940af4065d7b6a9932dc39
William Cahill <drewlikesramen@gmail.com>
2021-10-22 14:33:45 -0400

    Refactored graphics code into own module

---
+++ src/graphics/mod.rs
@@ -0,0 +1,9 @@
+mod mesh;
+mod renderer;
+mod texture;
+mod vertex;
+
+pub use mesh::*;
+pub use renderer::*;
+pub use texture::*;
+pub use vertex::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,2 @@
-mod vertex;
-mod mesh;
-mod renderer;
-mod texture;
-
 pub mod gui;
 pub mod voxel;
-pub use vertex::*;
-pub use mesh::*;
-pub use renderer::*;
-pub use texture::*;
