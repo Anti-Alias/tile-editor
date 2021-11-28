a0874d865bc513bcdc3f33a6537da6fdcb93b484
William Cahill <drewlikesramen@gmail.com>
2021-11-27 23:01:55 -0500

    Renaming renderer

--- src/graphics/screen/mod.rs
+++ src/graphics/screen/mod.rs
@@ -1,5 +1,5 @@
 mod screen_buffer;
-mod gbuffer_point_light_renderer;
+mod point_light_renderer;
 
 pub use screen_buffer::*;
-pub use gbuffer_point_light_renderer::*;
+pub use point_light_renderer::*;
